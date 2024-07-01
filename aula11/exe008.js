8776cbd535ff81ae0fa1db2fcedd6ee8fff79dff
Ermeson Ramos de Lima <ermesonramos1234@gmail.com>
2024-07-01 15:28:58 -0300

    Estudando condições em JS (Simples e Composta)

---
+++ aula11/exe008.js
@@ -0,0 +1,8 @@
+var velocidade = 100.5
+console.log(`A velocidade do seu carro é ${velocidade} km/h`)
+
+//Condição Simples
+if (velocidade > 60){
+    console.log('Voçê ultrapassou a velocidade permitida. MULTADO')
+}
+console.log('Dirija sempre usando cinto de segurança')
